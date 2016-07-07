4d1151c9fd4b510380aad54a465986626d1ee35d
TheAbhilash <mund.abhilash@gmail.com>
2016-07-07 20:07:40 +0530

    first commit

---
+++ test/src/test/Rus.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Rus {
+
+	public static void main(String[] args) {
+		String s = "abcaccgstrda";
+		
+		String s2 = s.replace('a', 'A');
+		
+		System.out.println(s2);
+	}
+}
